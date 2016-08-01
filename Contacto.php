046d827f3f898cb37a1feb2c57e4fb446805cef0
VICTOR LIMA <vituxofm@gmail.com>
2016-08-01 10:16:33 -0500

    CONEXION BD

---
+++ Contacto.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Contacts</title>
+<meta charset="utf-8">
+<link rel="stylesheet" href="css/reset.css" type="text/css" media="all">
+<link rel="stylesheet" href="css/layout.css" type="text/css" media="all">
+<link rel="stylesheet" href="css/style.css" type="text/css" media="all">
+<script type="text/javascript" src="js/jquery-1.4.2.js" ></script>
+<script type="text/javascript" src="js/cufon-yui.js"></script>
+<script type="text/javascript" src="js/cufon-replace.js"></script>
+<script type="text/javascript" src="js/CabinSketch_700.font.js"></script>
+<script type="text/javascript" src="js/EB_Garamond_400.font.js"></script>
+<!-- [if lt IE 9]>
+<script type="text/javascript" src="js/html5.js"></script>
+<style type="text/css">
+.bg {behavior:url(js/PIE.htc)}
+</style>
+<![endif]-->
+<!-- [if lt IE 7]>
+<div style='clear:both;text-align:center;position:relative'>
+	<a href="http://www.microsoft.com/windows/internet-explorer/default.aspx?ocid=ie6_countdown_bannercode"><img src="http://www.theie6countdown.com/images/upgrade.jpg" border="0" alt="" /></a>
+</div>
+<![endif]-->
+</head>
+<body id="page5">
+<div class="main">
+<!-- header -->
+	<header>
+		<div class="wrapper">
+			<nav>
+				<ul id="menu">
+					<li><a href="index.html"><span>Inicio</span></a></li>
+					<li><a href="Gallery.php"><span>Gallery</span></a></li>
+					<li id="menu_active"><a href="Contacto.php"><span>Contacto</span></a></li>
+				</ul>
+			</nav>
+		</div>
+				<h1><a href="index.html" >Reconocimiento de Rostro</a></h1>
+			</header>
+<!-- / header -->
+<!-- content -->
+	<section id="content">
+		<div class="wrapper">
+			<article class="col1">
+				<h2>INFORMACIÓN</h2>
+				<div class="pad">
+					<div class="wrapper">
+						<p class="cols"><strong>País:<br>
+						Región:<br>
+					Provincia:<br>
+							Email:</strong>
+						</p>
+						<p class="col3">PERÚ<br>
+					    Apurímac<br>
+						  Andahuaylas<br>
+							<a href="mailto:">vituxo@hotmail.com</a>
+						</p>
+						<p>.</p>
+					</div>
+				</div>
+				<h2><span>Contact Form</span></h2>
+				<form id="ContactForm">
+					<div>
+						<div class="wrapper">
+							<span>Nombre:</span>
+						  <input type="text" class="input">
+						</div>
+						<div class="wrapper">
+							<span>E-mail:</span><input type="text" class="input" >
+						</div>
+						<div class="textarea_box">
+							<span>Mensaje:</span>
+							<textarea name="textarea" cols="1" rows="1"></textarea>
+						</div>
+						<a href="#" onClick="document.getElementById('ContactForm').submit()">Send</a>
+					</div>
+				</form>
+    		</article>
+			<article class="col2">
+				<h3>Latest Works</h3>
+				<ul class="ul_works">
+					<li><a href="#"><img src="images/page1_img1.png" alt=""></a></li>
+					<li><a href="#"><img src="images/page1_img2.png" alt=""></a></li>
+				</ul>
+				<h4>Recent Tweets</h4>
+				<div class="tweets">
+					<p>País: Perú</p>
+					<p>Región: Apurimac.</p>
+					<p>Provincia: Andahuaylas.</p>
+					<p>Email:<strong><a href="mailto:">vitu@hotmail.com</a></strong></p>
+              </div>
+    		</article>
+		</div>
+	</section>
+<!-- / content -->
+<!-- footer -->
+	<footer>
+		<div class="wrapper"></div>
+		<p>&nbsp;</p>
+		<p>&nbsp;</p>
+		<p><a href="index.html" class="footer_logo">vitu<span>@hotmail</span>.com</a></p>
+    </footer>
+<!-- / footer -->
+</div>
+<script type="text/javascript">Cufon.now();</script>
+</body>
+</html>
