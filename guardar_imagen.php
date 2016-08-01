8ebfc6470776c1ef8fcc53057bb36a2fe5904b24
VICTOR LIMA <vituxofm@gmail.com>
2016-08-01 10:47:40 -0500

    GUARDAR IMAGEN CONCLUIDO

--- guardar_imagen.php
+++ guardar_imagen.php
@@ -4,5 +4,14 @@ include("conexion.php");
   $consulta="insert into fotos values ('$id_foto','','')";
   $inserta_foto=mysql_query($consulta,$c);
   $filename = "fotos/".$id_foto.'.jpg';
+  $result = file_put_contents( $filename, file_get_contents('php://input') );
+  if (!$result) 
+   {
+    	print "No se pudo subir la BD \n";
+	   exit();
+  }
+  $url = 'http://' . $_SERVER['HTTP_HOST'] . dirname($_SERVER['REQUEST_URI']) . '/' . $filename;
+  print "$url\n";
+
   
 ?>
