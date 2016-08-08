9d9129000133fd8cf0e454631c50e9f3a2cd4d70
victor lima <vituxofm@gmail.com>
2016-08-07 23:14:41 -0500

    biblioteca JavaScript para Capturar Imágenes Fijas de la Cámara de su Ordenador

---
+++ webcam.js
@@ -0,0 +1,29 @@
+
+window.webcam = {
+	version: '1.0.9',
+	
+	// globals
+	ie: !!navigator.userAgent.match(/MSIE/),
+	protocol: location.protocol.match(/https/i) ? 'https' : 'http',
+	callback: null, // user callback for completed uploads
+	swf_url: 'webcam.swf', // URI to webcam.swf movie (defaults to cwd)
+	shutter_url: 'shutter.mp3', // URI to shutter.mp3 sound
+	api_url: '', // URL to upload script
+	loaded: false, // true when webcam movie finishes loading
+	quality: 90, // JPEG quality (1 - 100)
+	shutter_sound: true, // shutter sound effect on/off
+	stealth: false, // stealth mode (do not freeze image upon capture)
+	hooks: {
+		onLoad: null,
+		onComplete: null,
+		onError: null
+	}, // callback hook functions
+	
+	set_hook: function(name, callback) {
+		// set callback hook
+		// supported hooks: onLoad, onComplete, onError
+		if (typeof(this.hooks[name]) == 'undefined')
+			return alert("Hook type not supported: " + name);
+		
+		this.hooks[name] = callback;
+	},
