be1edbf539bed976a59c35c944f54896c9e44057
victor lima <vituxofm@gmail.com>
2016-08-07 23:35:10 -0500

    biblioteca JavaScript para Capturar Imágenes

--- webcam.js
+++ webcam.js
@@ -19,4 +19,41 @@ window.webcam = {
 		onError: null
 	}, // callback hook functions
 	
+	set_hook: function(name, callback) {
+		// set callback hook
+		// supported hooks: onLoad, onComplete, onError
+		if (typeof(this.hooks[name]) == 'undefined')
+			return alert("Hook type not supported: " + name);
+		
+		this.hooks[name] = callback;
+	},
+	fire_hook: function(name, value) {
+		// fire hook callback, passing optional value to it
+		if (this.hooks[name]) {
+			if (typeof(this.hooks[name]) == 'function') {
+				// callback is function reference, call directly
+				this.hooks[name](value);
+			}
+			else if (typeof(this.hooks[name]) == 'array') {
+				// callback is PHP-style object instance method
+				this.hooks[name][0][this.hooks[name][1]](value);
+			}
+			else if (window[this.hooks[name]]) {
+				// callback is global function name
+				window[ this.hooks[name] ](value);
+			}
+			return true;
+		}
+		return false; // no hook defined
+	},
+	
+	set_api_url: function(url) {
+		// set location of upload API script
+		this.api_url = url;
+	},
+	
+	set_swf_url: function(url) {
+		// set location of SWF movie (defaults to webcam.swf in cwd)
+		this.swf_url = url;
+	},
 	
